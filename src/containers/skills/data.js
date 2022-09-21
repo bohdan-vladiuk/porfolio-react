3cd1395b7da35049ba213b4514b18a18d0535cc0
developer <codedreams9873@gmail.com>
2022-09-22 01:54:37 +0800

    added skills page

---
+++ src/containers/skills/data.js
@@ -0,0 +1,61 @@
+export const skillsData = [
+  {
+    title: "Front End",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: 90,
+      },
+      {
+        skillName: "CSS",
+        percentage: 90,
+      },
+      {
+        skillName: "Javascript",
+        percentage: 90,
+      },
+      {
+        skillName: "Typescript",
+        percentage: 90,
+      },
+    ],
+  },
+  {
+    title: "Back End",
+    data: [
+      {
+        skillName: "Node.js",
+        percentage: 90,
+      },
+      {
+        skillName: "Python",
+        percentage: 90,
+      },
+      {
+        skillName: "Express.js",
+        percentage: 90,
+      },
+      {
+        skillName: "Django",
+        percentage: 90,
+      },
+    ],
+  },
+  {
+    title: "Database",
+    data: [
+      {
+        skillName: "MySQL",
+        percentage: 90,
+      },
+      {
+        skillName: "MongoDB",
+        percentage: 90,
+      },
+      {
+        skillName: "PostgreSQL",
+        percentage: 90,
+      },
+    ],
+  },
+];
--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { GiSkills } from "react-icons/gi";
+import PageHeader from "../../components/pageHeader";
+import { Line } from "rc-progress";
+import { skillsData } from "./data";
+import "./styles.scss";
 
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <section id="skills" className="skills">
+      <PageHeader headerText="My skills" icon={<GiSkills size={40} />} />
+      <div className="skills__wrapper">
+        {skillsData.map((item, index) => (
+          <div key={index} className="skills__wrapper__content">
+            <h3 className="skills__wrapper__content__title">{item.title}</h3>
+            <div>
+              {item.data.map((dataItem, index) => (
+                <div className="progressbar-wrapper" key={index}>
+                  <p>{dataItem.skillName}</p>
+                  <Line
+                    percent={dataItem.percentage}
+                    strokeWidth="2"
+                    strokeColor="var(--selected-theme-main-color)"
+                    trailWidth="2"
+                  ></Line>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Skills;
